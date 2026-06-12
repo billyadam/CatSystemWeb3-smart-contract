7ac459d62dbe8e142f3e587701e781a4c51298e6
vinsa61 <vinsarioshen610@gmail.com>
2026-06-12 18:04:37 +0700

    refactor(cat): refactor cat data

--- programs/cat_system/src/state/cat.rs
+++ programs/cat_system/src/state/cat.rs
@@ -5,7 +5,9 @@ pub const MAX_DESC_LEN: usize = 512;
 pub const MAX_BREED_LEN: usize = 32;
 pub const MAX_COAT_COLOR_LEN: usize = 32;
 pub const MAX_EYE_COLOR_LEN: usize = 32;
+pub const MAX_PERSONALITY_TRAIT_LEN: usize = 32;
 pub const MAX_IMAGE_URL_LEN: usize = 256;
+pub const MAX_IMAGE_COUNT_LEN: u8 = 255;
 
 #[account]
 #[derive(InitSpace)]
@@ -15,6 +17,28 @@ pub struct Cat {
     #[max_len(32)]
     pub name: String,
     pub gender: Gender,
+    pub bio_profile: BioProfile,
+    pub image_count: u8,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct CatImage {
+    pub cat: Pubkey,
+    pub bump: u8,
+    pub index: u8,
+    #[max_len(256)]
+    pub image_url: String,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, InitSpace)]
+pub enum Gender {
+    Male,
+    Female,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, InitSpace)]
+pub struct BioProfile {
     #[max_len(32)]
     pub breed: String,
     #[max_len(32)]
@@ -24,18 +48,10 @@ pub struct Cat {
     pub eye_color: String,
     pub ear_type: EarType,
     pub body_size: BodySize,
+    #[max_len(32)]
+    pub personality_trait: String,
     #[max_len(512)]
     pub description: String,
-    #[max_len(256)]
-    pub image_url_1: String,
-    #[max_len(256)]
-    pub image_url_2: String,
-}
-
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, InitSpace)]
-pub enum Gender {
-    Male,
-    Female,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, InitSpace)]
